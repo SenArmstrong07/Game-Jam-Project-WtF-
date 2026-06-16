f04baf1a16198f222bcd69eab839ecce1fde8935
GreggMays <108717496+GreggMays@users.noreply.github.com>
2026-06-16 11:12:42 +0800

    some file rearrangements

--- scripts/units/Enemy.gd
+++
@@ -1,9 +0,0 @@
-extends Unit #since we inherit from Unit, lahat ng properties and functions ni unit is applicable na dito.
-class_name Enemy
-
-var enemy_type
-
-func _ready():
-	team = Team.ENEMY
-
-	#JUST SET SOME STUFF HERE:
--- scripts/units/Player.gd
+++
@@ -1,8 +0,0 @@
-
-
-extends Unit #since we inherit from Unit, lahat ng properties and functions ni unit is applicable na dito.
-class_name Player
-
-
-func _ready():
-	team = Team.PLAYER
