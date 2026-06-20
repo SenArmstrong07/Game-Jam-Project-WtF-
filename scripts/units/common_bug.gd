b5521d72b03ecfe640ca4a49adb64ac5eb9a9a1e
Venice Belandres <166132397+Vineaszt@users.noreply.github.com>
2026-06-20 23:00:12 +0800

    Feature: Simple Enemy Movement + Attack
    enemy follows the player on the grid and shoots when detected in front of the player

--- scripts/units/common_bug.gd
+++ scripts/units/common_bug.gd
@@ -1,6 +1,10 @@
 extends Unit
 
 @onready var anim_player: AnimatedSprite2D = $AnimatedSprite2D
+const EnemyBasicProjectile = preload("res://scenes/Attacks/enemy_basic_projectile.tscn")
+@onready var ProjectileShootPoint: Marker2D = $Marker2D
+@onready var player_character: CharacterBody2D = $"../PlayerCharacter"
+
 
 const GRID_WIDTH := 4
 const GRID_HEIGHT := 4
@@ -10,8 +14,10 @@ const TILE_SIZE := 64
 var grid_pos := Vector2i(0, 0) 
 var target_position := Vector2.ZERO
 
-var move_timer := 0.0
-var move_interval := 1.0
+var follow_timer := 0.0
+var follow_interval := 1.0
+var shoot_timer := 0.0
+var shoot_interval := 1.0
 
 var move_speed := 200.0
 
@@ -28,30 +34,50 @@ func grid_to_world(cell: Vector2i) -> Vector2:
 	)
 
 func _process(delta):
-	move_timer += delta
+	shoot_timer += delta
+	follow_timer += delta
 
-	if move_timer >= move_interval:
-		move_timer = 0
-		choose_random_move()
+	if follow_timer >= follow_interval:
+		follow_timer = 0.0
+		follow_player()
 
-	# smooth movement
-	position = position.move_toward(target_position, move_speed * delta)
+	position = position.move_toward(
+		target_position,
+		move_speed * delta
+	)
+	if shoot_timer >= shoot_interval:
+		shoot_timer = 0
 
-func choose_random_move():
-	var directions = [
-		Vector2i(1, 0),
-		Vector2i(-1, 0),
-		Vector2i(0, 1),
-		Vector2i(0, -1)
-	]
+		if player_in_front():
+			shoot()
 
-	var dir = directions[randi() % directions.size()]
-	var new_pos = grid_pos + dir
+func follow_player():
+	if player_character == null:
+		return
 
-	new_pos.x = clamp(new_pos.x, 0, GRID_WIDTH - 1)
-	new_pos.y = clamp(new_pos.y, 0, GRID_HEIGHT - 1)
+	var player_row = player_character.grid_pos.y
 
-	if new_pos != grid_pos:
-		grid_pos = new_pos
-		target_position = grid_to_world(grid_pos)
-		anim_player.play("Move")
+	if player_row > grid_pos.y:
+		grid_pos.y += 1
+	elif player_row < grid_pos.y:
+		grid_pos.y -= 1
+
+	grid_pos.y = clamp(grid_pos.y, 0, GRID_HEIGHT - 1)
+
+	target_position = grid_to_world(grid_pos)
+		
+		
+func player_in_front() -> bool:
+	return (
+		player_character.grid_pos.y == grid_pos.y
+		and player_character.global_position.x < global_position.x
+	)
+	
+#shoot funtion sets the postion of the projectile with marker2d
+func shoot():
+	var projectile = EnemyBasicProjectile.instantiate()
+
+	projectile.global_position = ProjectileShootPoint.global_position
+	projectile.direction = Vector2.LEFT
+
+	get_tree().current_scene.add_child(projectile)
